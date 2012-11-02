27bdf2b542a74c4834167bbf66c27510da2b450b
Pat Shea <patshea56@gmail.com>
2012-11-02 06:57:51 -0400

    Even more jade

--- sandbox/bubbleNewTest/app.js
+++ sandbox/bubbleNewTest/app.js
@@ -29,11 +29,8 @@ app.configure('production', function(){
 
 // Routes
 
-app.get('/', routes.index);
-app.post('/', routes.index_post_handler);
-
-app.get('/bubbles', routes.bubbles);
-app.get('/bubbles/:id', routes.bubble);
+app.get('/', routes.bubble);
+app.get('/:id', routes.bubble);
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- sandbox/bubbleNewTest/routes/index.js
+++ sandbox/bubbleNewTest/routes/index.js
@@ -9,30 +9,16 @@ var bubbles = {
     sports:{name:'sports', slug: 'sports'}
 };
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Emory Bubble', bubbles_list: bubbles })
-};
-
-// handler for form submitted from homepage
-exports.index_post_handler = function(req, res){
-  // if the username is not submitted, give it a default of "Anonymous"
-    username = req.body.username || 'Anonymous';
-  // redirect the user to homepage
-    res.redirect('/bubbles');
-};
-
-
-
-
-
-// handler for displaying list of bubbles
-exports.bubbles = function(req, res) {
-    res.render('bubbles', { title: 'Emory Bubble - Select a Bubble', bubbles_list: bubbles });
-};
 
 // handler for displaying individual bubble
 exports.bubble = function(req, res) {
-    var name = bubbles[req.params.id].name;
-    var slug = bubbles[req.params.id].slug;
+    if (bubbles[req.params.id])
+      var selected_bubble = bubbles[req.params.id];
+    else
+      var selected_bubble = bubbles['night_life'];
+
+    var name = selected_bubble.name;
+    var slug = selected_bubble.slug;
+
     res.render('bubble', { title: 'Emory Bubble - ' + name, bubbles_list: bubbles, name: name, slug: slug });
 };
