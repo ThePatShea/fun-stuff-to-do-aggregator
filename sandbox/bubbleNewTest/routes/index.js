c3cf9b48c2c9648a8f0d8033d540735b01b3d895
Pat Shea <patshea56@gmail.com>
2012-11-02 00:20:35 -0400

    Continued express and jade

--- sandbox/bubbleNewTest/routes/index.js
+++ sandbox/bubbleNewTest/routes/index.js
@@ -6,3 +6,13 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Emory Bubble' })
 };
+
+// handler for form submitted from homepage
+exports.index_post_handler = function(req, res){
+  // if the username is not submitted, give it a default of "Anonymous"
+    username = req.body.username || 'Anonymous';
+  // store the username as a session variable
+    req.session.username = username;
+  // redirect the user to homepage
+    res.redirect('/');
+};
