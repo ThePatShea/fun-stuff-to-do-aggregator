6a00accafa50008f735e4d09a227675ca4935dd2
Pat Shea <patshea56@gmail.com>
2012-11-04 02:59:25 -0500

    Lots more interface building in Jade

--- sandbox/bubbleNewTest/app.js
+++ sandbox/bubbleNewTest/app.js
@@ -30,7 +30,9 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.bubble);
-app.get('/:id', routes.bubble);
+app.get('/:bubble_slug', routes.bubble);
+app.get('/:bubble_slug/:post_id/:post_slug', routes.post);
+
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- sandbox/bubbleNewTest/routes/index.js
+++ sandbox/bubbleNewTest/routes/index.js
@@ -1,5 +1,75 @@
 var bubbles = {
-    night_life:{name:'night life', slug: 'night_life'},
+    night_life:{
+      name:'night life',
+      slug: 'night_life',
+      events: {
+        1: {
+          bubble_id: '1',
+          slug: 'big_party_at_opera',
+          name: 'big party at opera',
+          location: 'opera nightclub',
+          full_time: 'thursday, november 1st | 10:00pm - 1:00am',
+          description: 'College Night is Wednesdays ultimate destination for the citys most beautiful party people. College Night is Wednesdays ultimate destination for the citys most beautiful party people. College Night is Wednesdays ultimate destination for the citys most beautiful party people. College Night is Wednesdays ultimate destination for the citys most beautiful party people. College Night is Wednesdays ultimate destination for the citys most beautiful party people. College Night is Wednesdays ultimate destination for the citys most beautiful party people. College Night is Wednesdays ultimate destination for the citys most beautiful party people. College Night is Wednesdays ultimate destination for the citys most beautiful party people. College Night is Wednesdays ultimate destination for the citys most beautiful party people. College Night is Wednesdays ultimate destination for the citys most beautiful party people. College Night is Wednesdays ultimate destination for the citys most beautiful party people. College Night is Wednesdays ultimate destination for the citys most beautiful party people. College Night is Wednesdays ultimate destination for the citys most beautiful party people. College Night is Wednesdays ultimate destination for the citys most beautiful party people. College Night is Wednesdays ultimate destination for the citys most beautiful party people. College Night is Wednesdays ultimate destination for the citys most beautiful party people. College Night is Wednesdays ultimate destination for the citys most beautiful party people. College Night is Wednesdays ultimate destination for the citys most beautiful party people. College Night is Wednesdays ultimate destination for the citys most beautiful party people. College Night is Wednesdays ultimate destination for the citys most beautiful party people. College Night is Wednesdays ultimate destination for the citys most beautiful party people. College Night is Wednesdays ultimate destination for the citys most beautiful party people. College Night is Wednesdays ultimate destination for the citys most beautiful party people. College Night is Wednesdays ultimate destination for the citys most beautiful party people. College Night is Wednesdays ultimate destination for the citys most beautiful party people. College Night is Wednesdays ultimate destination for the citys most beautiful party people.',
+          pic_big: 'https://fbcdn-profile-a.akamaihd.net/hprofile-ak-ash4/276429_103881343106322_1983802727_n.jpg'
+        },
+        2: {
+          bubble_id: '2',
+          slug: 'butch_clancy_terminal_west',
+          name: 'butch clancy @ terminal west',
+          location: 'terminal west',
+          full_time: 'thursday, november 1st | 10:00pm - 1:00am', 
+          pic_big: 'https://fbcdn-profile-a.akamaihd.net/hprofile-ak-snc6/188168_418693628185481_517348357_n.jpg'
+        },
+        3: {
+          bubble_id: '3',
+          slug: 'back_to_school_bash',
+          name: 'back to school bash',
+          location: 'mansion elan',
+          full_time: 'thursday, november 1st | 10:00pm - 1:00am',
+          pic_big: 'https://fbcdn-profile-a.akamaihd.net/hprofile-ak-snc6/276674_124771344342841_1247972548_n.jpg'
+        },
+        4: {
+          bubble_id: '4',
+          slug: 'college_night_with_the_bros',
+          name: 'college night with the bros',
+          location: 'flip flops',
+          full_time: 'thursday, november 1st | 10:00pm - 1:00am',
+          pic_big: 'https://fbcdn-profile-a.akamaihd.net/hprofile-ak-snc6/188148_123889957765084_764900719_n.jpg'
+        },
+        5: {
+          bubble_id: '5',
+          slug: 'big_party_at_opera',
+          name: 'big party at opera',
+          location: 'opera nightclub',
+          full_time: 'thursday, november 1st | 10:00pm - 1:00am',
+          pic_big: 'https://fbcdn-profile-a.akamaihd.net/hprofile-ak-ash4/276429_103881343106322_1983802727_n.jpg'
+        },
+        6: {
+          bubble_id: '6',
+          slug: 'butch_clancy_terminal_west',
+          name: 'butch clancy @ terminal west',
+          location: 'terminal west',
+          full_time: 'thursday, november 1st | 10:00pm - 1:00am',
+          pic_big: 'https://fbcdn-profile-a.akamaihd.net/hprofile-ak-snc6/188168_418693628185481_517348357_n.jpg'
+        },
+        7: {
+          bubble_id: '7',
+          slug: 'back_to_school_bash',
+          name: 'back to school bash',
+          location: 'mansion elan',
+          full_time: 'thursday, november 1st | 10:00pm - 1:00am',
+          pic_big: 'https://fbcdn-profile-a.akamaihd.net/hprofile-ak-snc6/276674_124771344342841_1247972548_n.jpg'
+        },
+        8: {
+          bubble_id: '8',
+          slug: 'college_night_with_the_bros',
+          name: 'college night with the bros',
+          location: 'flip flops',
+          full_time: 'thursday, november 1st | 10:00pm - 1:00am',
+          pic_big: 'https://fbcdn-profile-a.akamaihd.net/hprofile-ak-snc6/188148_123889957765084_764900719_n.jpg'
+        },
+      }
+    },
     campus_events:{name:'campus events', slug: 'campus_events'},
     student_deals:{name:'student deals', slug: 'student_deals'},
     concerts:{name:'concerts', slug: 'concerts'},
@@ -13,12 +83,24 @@ var bubbles = {
 // handler for displaying individual bubble
 exports.bubble = function(req, res) {
     if (bubbles[req.params.id])
-      var selected_bubble = bubbles[req.params.id];
+      var selected_bubble = bubbles[req.params.bubble_slug];
     else
       var selected_bubble = bubbles['night_life'];
 
     var name = selected_bubble.name;
     var slug = selected_bubble.slug;
+    
+    var selected_post = "";
 
-    res.render('bubble', { title: 'Emory Bubble - ' + name, bubbles_list: bubbles, name: name, slug: slug });
+    res.render('bubble', { title: 'Emory Bubble - ' + name, bubbles_list: bubbles, name: name, slug: slug, post: selected_post});
+};
+
+exports.post = function(req, res) {
+    var selected_bubble = bubbles[req.params.bubble_slug];
+    var selected_post = bubbles[req.params.bubble_slug].events[req.params.post_id];
+
+    var name = selected_bubble.name;
+    var slug = selected_bubble.slug;
+
+    res.render('bubble', { title: 'Emory Bubble - ' + name, bubbles_list: bubbles, name: name, slug: slug, post: selected_post});
 };
