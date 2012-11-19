13ec5a3089bb5c2acfd5762725810f94df6037d4
Campus Bubble <development@thecampusbubble.com>
2012-11-18 22:30:53 -0500

    Got the arts and music bubble sorted

--- sandbox/bubble3/curate/sort.js
+++ sandbox/bubble3/curate/sort.js
@@ -5,3 +5,4 @@
 // Sort bubbles
 	base_mongo.sort_bubble("night life" , {$or : [{"type": /night/i} , {"categories.name": /night/i}] , "location.state" : "GA"});
 	base_mongo.sort_bubble("concerts" , {$or : [{"type": /concert/i} , {"categories.name": /concert/i}] , "location.state" : "GA"});
+	base_mongo.sort_bubble("arts and music" , {$or : [{"type": /art/i} , {"categories.name": /art/i}] , "location.state" : "GA"});
