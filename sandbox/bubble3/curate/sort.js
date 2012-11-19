54ca6156edfa1a12cd7ecbafc44288f8e71cf113
Campus Bubble <development@thecampusbubble.com>
2012-11-18 22:04:27 -0500

    Revert "Messed up sort.js. Trying to revert to earlier commmit"
    This reverts commit 28a74c8a04eb34707143a6b4a13f14f01f8f640c.

--- sandbox/bubble3/curate/sort.js
+++ sandbox/bubble3/curate/sort.js
@@ -6,14 +6,12 @@
 	//TODO: Campus Events: Make it fit the parameters I wrote down on the sheet of paper
 	//TODO: Concerts/Night Life: Look for the words "concert/night life/nightlife" in their page names/descriptions AND their event names/descriptions/locations
 	//TODO: Fix the query for the Arts & Music Bubble so it gets more relevant events
-	//TODO: Fix the glitch that is causing all the bubbles except Campus Events to grab way too many events
-//	base_mongo.sort_bubbles("night life", {$or : [{"type" : /night/i} , {"categories.name" : /night/i}] , "location.state" : "GA"});
-//	base_mongo.sort_bubbles("concerts", {$or : [{"type" : /concert/i} , {"categories.name" : /concert/i}] , "location.state" : "GA"});
-//	base_mongo.sort_bubbles("campus events", {$or : [{name : /Emory/} , {description : /Emory/}]}, {$or : [{name : /Emory/} , {description : /Emory/} , {location : /Emory/}]});
+	base_mongo.sort_bubbles("night life", {$or : [{"type" : /night/i} , {"categories.name" : /night/i}] , "location.state" : "GA"});
+	base_mongo.sort_bubbles("concerts", {$or : [{"type" : /concert/i} , {"categories.name" : /concert/i}] , "location.state" : "GA"});
+	base_mongo.sort_bubbles("campus events", {$or : [{name : /Emory/} , {description : /Emory/}]}, {$or : [{name : /Emory/} , {description : /Emory/} , {location : /Emory/}]});
 	
 
-	base_mongo.sort_bubbles("arts and music", {page_id: 0}, {eid: 0});
-
+	base_mongo.sort_bubbles("arts and music", {$or : [{"type" : /art/i} , {"categories.name" : /art/i} , {"name" : /art/i}] , "location.state" : "GA"});
 
 
 
