0ba9a60d0d13ae06b753c8580814585b67cd13d9
Campus Bubble <development@thecampusbubble.com>
2012-11-18 22:08:33 -0500

    Revert "Got most minimally viable bubbles working in sort.js"
    This reverts commit f77adcef806a27449d4935afe49086b42995e1f5.

--- sandbox/bubble3/curate/sort.js
+++ sandbox/bubble3/curate/sort.js
@@ -1,16 +1,6 @@
 // Require base scripts
         var base_mongo = require("./javascripts/base_mongo.js");
 
-
-// Sort bubbles
-	//TODO: Campus Events: Make it fit the parameters I wrote down on the sheet of paper
-	//TODO: Concerts/Night Life: Look for the words "concert/night life/nightlife" in their page names/descriptions AND their event names/descriptions/locations
-	base_mongo.sort_bubbles("night life", {$or : [{"type" : /night/i} , {"categories.name" : /night/i}] , "location.state" : "GA"});
-	base_mongo.sort_bubbles("concerts", {$or : [{"type" : /concert/i} , {"categories.name" : /concert/i}] , "location.state" : "GA"});
-	base_mongo.sort_bubbles("campus events", {$or : [{name : /Emory/} , {description : /Emory/}]}, {$or : [{name : /Emory/} , {description : /Emory/} , {location : /Emory/}]});
-	base_mongo.sort_bubbles("night life", {$or : [{"type" : /night/i} , {"categories.name" : /night/i}] , "location.state" : "GA"});
-	base_mongo.sort_bubbles("arts and music", {$or : [{"type" : /art/i} , {"categories.name" : /art/i} , {"name" : / art/i} , {"description" : / art/i}] , "location.state" : "GA"});
-
-
-
-
+	base_mongo.find_mongo_info("event", function(mongo_model) {
+		console.log(mongo_model);
+	});
