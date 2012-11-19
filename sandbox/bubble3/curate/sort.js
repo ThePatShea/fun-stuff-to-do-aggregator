7b59a4366bc5fbe694334cfd419fe35c2633883b
Campus Bubble <development@thecampusbubble.com>
2012-11-18 22:41:34 -0500

    Finished minimally viable sort.js. We should improve sort.js before launch though.

--- sandbox/bubble3/curate/sort.js
+++ sandbox/bubble3/curate/sort.js
@@ -3,7 +3,12 @@
 
 
 // Sort bubbles
-	base_mongo.sort_bubble("night life" , {$or : [{"type": /night/i} , {"categories.name": /night/i}] , "location.state" : "GA"});
-	base_mongo.sort_bubble("concerts" , {$or : [{"type": /concert/i} , {"categories.name": /concert/i}] , "location.state" : "GA"});
-	base_mongo.sort_bubble("arts and music" , {$or : [{"type": /art/i} , {"categories.name": /art/i}] , "location.state" : "GA"});
-	base_mongo.sort_bubble("campus events" , {$or : [{"type": /Emory/} , {"description": /Emory/}] , "location.state" : "GA"});
+	function bubble_sort() {
+		base_mongo.sort_bubble("concerts" , {$or : [{"type": /concert/i} , {"categories.name": /concert/i}] , "location.state" : "GA"});
+		base_mongo.sort_bubble("night life" , {$or : [{"type": /night/i} , {"categories.name": /night/i}] , "location.state" : "GA"});
+		base_mongo.sort_bubble("arts and music" , {$or : [{"type": /art/i} , {"categories.name": /art/i}] , "location.state" : "GA"});
+		base_mongo.sort_bubble("campus events" , {$or : [{"type": /Emory/} , {"description": /Emory/}] , "location.state" : "GA"});
+	}
+
+// Run the sort function
+	bubble_sort();
